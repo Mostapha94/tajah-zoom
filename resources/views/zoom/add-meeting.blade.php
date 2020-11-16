8da60d88df0d47008bb4e1852ba1d2caa4b54c9b
Mostapha94 <mostapha.mohamed@mahacode.com>
2020-11-17 00:14:54 +0200

    datetime input

--- resources/views/zoom/add-meeting.blade.php
+++ resources/views/zoom/add-meeting.blade.php
@@ -6,6 +6,11 @@
     <title>Create meeting</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+    <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
+    <script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
   </head>
 
   <body class="gray-background rtl">
@@ -29,7 +34,7 @@
                                 <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
                                 <div class="col-md-8">
                                 <label>Start time</label><br>
-                                <input type="timedate" id="birthdaytime" name="start_time"> <br>
+                                <input type="text" id="dtpickerdemo" name="start_time"> <br>
                             </div>
                             <div class="form-group">
                                 <button type="submit" class="btn btn-primary">Save</button>
@@ -69,5 +74,10 @@
             </div>   
         </div>
     </div>
+    <script type="text/javascript">
+            $(function () {
+                $('#dtpickerdemo').datetimepicker();
+            });
+    </script>
   </body>
 </html>
